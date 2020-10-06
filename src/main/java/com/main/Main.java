c7c1bfeea32e6b00488dc4ec33c2d4e35d8d92ae
FachryRizano <fachry.rizano@gmail.com>
2020-10-06 21:01:57 +0700

    Menambah fitur otomatis untuk membuka URL dari produk termurah yang dicari berdasarkan input

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,7 +1,5 @@
 package com.main;
 
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,6 +7,7 @@ import org.jsoup.select.Elements;
 
 import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,7 +21,7 @@ public class Main {
         String input= scan.nextLine();
         String barang = input.replaceAll("\\s","%20");
         String url = "https://www.tokopedia.com/search?st=product&q=" +barang;
-        Desktop desktop = java.awt.Desktop.getDesktop();
+
 
         doc = Jsoup.connect(url).get();
         Elements elements = doc.getElementsByClass("pcv3__container css-1bd8ct");
@@ -33,10 +32,16 @@ public class Main {
             String link = e.getElementsByClass("css-1ehqh5q").select("a").attr("href");
             Produk produk = new Produk(name,harga,link);
             produks.add(produk);
-            System.out.println(link);
+        }
+        Collections.sort(produks);
+        Desktop desktop = java.awt.Desktop.getDesktop();
+        try{
+            URI oURL = new URI(produks.get(0).getUrl());
+            desktop.browse(oURL);
+        }catch(Exception e){
+            System.err.println(e);
         }
 
-        Collections.sort(produks);
 
     }
 
